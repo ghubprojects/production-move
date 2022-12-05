487348a8cac0452365d795fab5c6d0214f4f42e6
ghubprojects <anhtheit2002@gmail.com>
2022-12-05 09:34:33 +0700

    gitignore .vscode

--- server/index.js
+++ server/index.js
@@ -20,7 +20,7 @@ db.sequelize.sync();
 
 // simple route
 app.get('/', (req, res) => {
-    res.json({ message: 'Welcome to bezkoder application.' });
+    res.json({ message: 'Welcome to production-move application.' });
 });
 
 require('./app/routes/tutorial.routes')(app);
