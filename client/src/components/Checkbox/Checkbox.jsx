64176a41fcf2ce55a67a8dadee5941925af68518
Phạm Bích Ngọc <20020454@vnu.edu.vn>
2022-12-29 17:18:38 +0700

    checkbox handleChange

--- client/src/components/Checkbox/Checkbox.jsx
+++ client/src/components/Checkbox/Checkbox.jsx
@@ -2,13 +2,30 @@ import PropTypes from 'prop-types';
 import { Link } from 'react-router-dom';
 import classNames from 'classnames/bind';
 import styles from './Checkbox.module.scss';
+import { useState } from 'react';
 
 const cx = classNames.bind(styles);
 
 function Checkbox({ id }) {
+    const [checked, setChecked] = useState(false);
+    const handleChange = (data) => {
+        if (!checked) {
+            setChecked(true);
+            console.log(data);
+        } else {
+            setChecked(false);
+        }
+    };
     return (
         <div className='container'>
-            <input type='checkbox' id={id} className={cx('check-box')}></input>
+            <input
+                type='checkbox'
+                id={id}
+                className={cx('check-box')}
+                onClick={() => {
+                    handleChange(id);
+                }}
+            ></input>
             <label for={id} className={cx('product-name')}>
                 {id}
             </label>
